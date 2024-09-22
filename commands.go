afcf016040c011eeb0c6090c60c18f2c78da8487
alisjj <aliyusajjads@gmail.com>
2024-09-23 00:25:00 +0100

    update

--- commands.go
+++ commands.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 )
@@ -9,26 +11,47 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func getCliCommands() map[string]cliCommand {
+type config struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func getCliCommands(*config) map[string]cliCommand {
 
 	return map[string]cliCommand{
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
-			callback:    commandHelp,
+			callback:    func(*config) error { return commandHelp() },
 		},
 		"clear": {
 			name:        "clear",
 			description: "Cleans the screen",
-			callback:    commandClear,
+			callback:    func(*config) error { return commandClear() },
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the names of 20 location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous names of 20 location areas",
+			callback:    commandMapB,
+		},
+
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
-			callback:    commandExit,
+			callback:    func(*config) error { return commandExit() },
 		},
 	}
 }
@@ -49,10 +72,66 @@ func commandHelp() error {
 	fmt.Println("\nWelcome to Pokedex!")
 	fmt.Println("Usage:")
 
-	for _, i := range getCliCommands() {
+	for _, i := range getCliCommands(nil) {
 		fmt.Printf("\n%v: %v", i.name, i.description)
 
 	}
 	fmt.Print("\n\n")
 	return nil
+}
+
+func commandMap(c *config) error {
+
+	var url string
+	if c.Next != nil {
+		url = *c.Next
+	} else {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&c)
+	if err != nil {
+		return err
+	}
+
+	for _, r := range c.Results {
+		fmt.Println(r.Name)
+	}
+
+	return nil
+
+}
+
+func commandMapB(c *config) error {
+
+	var url string
+	if c.Previous == nil {
+		return fmt.Errorf("there is not previous page\n")
+
+	}
+	url = *c.Previous
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&c)
+	if err != nil {
+		return err
+	}
+
+	for _, r := range c.Results {
+		fmt.Println(r.Name)
+	}
+
+	return nil
+
 }
--- repl.go
+++ repl.go
@@ -9,18 +9,19 @@ import (
 
 func repl() {
 	reader := bufio.NewScanner(os.Stdin)
+	cfg := &config{}
 	for {
 		fmt.Print("Pokedex> ")
 		reader.Scan()
 		text := reader.Text()
 		command := cleanInput(text)
 
-		if _, ok := getCliCommands()[command]; !ok {
+		if _, ok := getCliCommands(cfg)[command]; !ok {
 			fmt.Println("command not found")
 			continue
 		}
 
-		err := getCliCommands()[command].callback()
+		err := getCliCommands(cfg)[command].callback(cfg)
 		if err != nil {
 			fmt.Printf("Error: %v", err)
 			continue
